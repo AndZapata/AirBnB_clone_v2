6a0244f4f29a0e97b5f86036f29c2a5ffe11b0b5
AndZapata <andzapata514@gmail.com>
2019-09-03 18:18:31 +0000

    [Fix] pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,15 +3,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     ''' display “Hello HBNB!” '''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     ''' display “HBNB” '''
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
@@ -19,6 +22,7 @@ def c_is_fun(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 @app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def py_is_cool(text='is_cool'):
     ''' display “HBNB” '''
     return 'Python {}'.format(text.replace('_', ' '))
