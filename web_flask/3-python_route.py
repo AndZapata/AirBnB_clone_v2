8f48b7edb3ba7fb65495845e734a89a552b16db1
AndZapata <andzapata514@gmail.com>
2019-09-03 18:19:33 +0000

    [Fix] pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c_is_fun(text):
     ''' display “HBNB” '''
     return 'C {}'.format(text.replace('_', ' '))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_is_cool(text='is_cool'):
