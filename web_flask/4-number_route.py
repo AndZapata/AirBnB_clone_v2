04d36554867b851d4e9a4b858d125e025638c3c7
AndZapata <andzapata514@gmail.com>
2019-09-03 18:48:51 +0000

    [Feature] number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' display “Hello HBNB!” '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' display “HBNB” '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    ''' display “HBNB” '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_is_cool(text='is_cool'):
+    ''' display “HBNB” '''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    ''' display “N” '''
+    return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
