e418a84282b548100a53dc4cb9f813cb1f3c38fd
AndZapata <andzapata514@gmail.com>
2019-09-03 18:01:13 +0000

    [Feature] starting flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' display “Hello HBNB!” '''
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' display “HBNB” '''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    ''' display “HBNB” '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
